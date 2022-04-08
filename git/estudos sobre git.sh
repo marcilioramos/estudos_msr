4591cabe6d126b2bf59cf245e1c41ce6710c5649
marcilioramos <48597831+marcilioramos@users.noreply.github.com>
2022-04-07 22:54:53 -0300

    Create estudos sobre git.sh

---
+++ git/estudos sobre git.sh
@@ -0,0 +1,11 @@
+#iniciar um projeto com o git
+git init
+
+#mostrar o status do trabalho
+git status
+
+#add arquivo ao controle de versão
+git add "nome do arquivo"
+ou 
+#adiciona tudo para versionamento
+git add . 
