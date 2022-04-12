0a781eda9419f42b8fb20576d7e06d0d65be89e4
marcilioramos <48597831+marcilioramos@users.noreply.github.com>
2022-04-12 00:00:02 -0300

    Update estudos sobre git.sh

--- git/estudos sobre git.sh
+++ git/estudos sobre git.sh
@@ -18,3 +18,26 @@ git config --global user.name "Your Name"
 
 #envia os codigos para nuvem
 git push
+
+#diz para que destino seu condigo está sendo enviado
+git remote add origin https://github.com/marcilioramos/estudos_msr
+
+#ver historico dos commits
+git reflog
+
+#retornar a uma versão do codigo, vc no caso vai restaurar um commit
+git reset --hard "codigo do commit" (esse codigo está disponivel ao rodar o comando git reflog)
+
+##muda a branch em que estão trabalhando 
+git checkout staging
+
+
+###branchens
+## essa branch geralmente é usado local onde estão recebando atualizações que ainda não estão sendo testadas
+## git branch staging
+
+
+
+
+
+
