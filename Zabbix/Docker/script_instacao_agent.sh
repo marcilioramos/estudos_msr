57c72f71e3d712e4c80feb6204c413f836083710
marcilioramos <48597831+marcilioramos@users.noreply.github.com>
2022-12-30 15:10:47 -0300

    Update script_instacao_agent.sh

--- Zabbix/Docker/script_instacao_agent.sh
+++ Zabbix/Docker/script_instacao_agent.sh
@@ -8,65 +8,55 @@
 #
 # Set the default values for the script arguments
 
-SERVER_IP=127.0.0.1
-SERVER_ACTIVE_IP=127.0.0.1
-HOSTNAME=`hostname`
+#!/bin/bash
 
-# Parse the script arguments
-while [[ $# -gt 0 ]]
-do
-    key="$1"
-    case $key in
-        -s|--server)
-            SERVER_IP="$2"
-            shift
-            shift
-            ;;
-        -a|--server-active)
-            SERVER_ACTIVE_IP="$2"
-            shift
-            shift
-            ;;
-        -h|--hostname)
-            HOSTNAME="$2"
-            shift
-            shift
-            ;;
-        *)
-            shift
-            ;;
-    esac
-done
-
-# Check if the system is Debian, Ubuntu, CentOS, or Red Hat
-if [ -f /etc/debian_version ]; then
-    # Install the Zabbix repository on Debian or Ubuntu
-    wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-1+focal_all.deb
-    dpkg -i zabbix-release_6.0-1+focal_all.deb
-    rm zabbix-release_6.0-1+focal_all.deb
-elif [ -f /etc/redhat-release ]; then
-    # Install the Zabbix repository on CentOS or Red Hat
-    rpm -Uvh https://repo.zabbix.com/zabbix/6.0/rhel/7/x86_64/zabbix-release-6.0-1.el7.noarch.rpm
+# Check distribution and version
+if [ -f /etc/redhat-release ]; then
+  # RHEL-based distribution
+  distro="rhel"
+  version=$(rpm -q --queryformat '%{VERSION}' centos-release)
+elif [ -f /etc/lsb-release ]; then
+  # Debian/Ubuntu-based distribution
+  distro=$(lsb_release -i | cut -d: -f2 | tr -d '[:space:]')
+  version=$(lsb_release -r | cut -d: -f2 | tr -d '[:space:]')
 else
-    # If the system is not recognized, exit the script
-    echo "This script only works on Debian, Ubuntu, CentOS, or Red Hat."
-    exit 1
+  # Unsupported distribution
+  echo "Error: unsupported distribution"
+  exit 1
 fi
 
-# Install the Zabbix agent2
-if [ -f /etc/debian_version ]; then
-    # Install the Zabbix agent2 on Debian or Ubuntu
-    apt-get update
-    apt-get install -y zabbix-agent2
-elif [ -f /etc/redhat-release ]; then
-    # Install the Zabbix agent2 on CentOS or Red Hat
-    yum update
-    yum install -y zabbix-agent2
+# Check if Zabbix Agent 2 is already installed
+if [ "$(dpkg-query -W -f='${Status}' zabbix-agent 2>/dev/null | grep -c "ok installed")" -eq 1 ]; then
+  echo "Zabbix Agent 2 is already installed"
+  exit 0
 fi
 
-# Backup the Zabbix agent2 configuration file
-cp /etc/zabbix/zabbix_agent2.conf /etc/zabbix/zabbix_agent2.conf.bak
+# Install dependencies
+if [ "$distro" = "rhel" ]; then
+  yum install -y wget
+elif [ "$distro" = "Debian" ] || [ "$distro" = "Ubuntu" ]; then
+  apt-get update
+  apt-get install -y wget
+else
+  echo "Error: unsupported distribution"
+  exit 1
+fi
 
-# Set the server and server active parameters in the Zabbix agent2 configuration file
-sed -i "s/Server=127.0.0.1/Server=$SERVER_IP/g" /etc/zabbix/zabbix_agent2.conf
-sed -i "s/ServerActive=127.0.0.1/ServerActive=$SERVER_ACTIVE_IP
+# Download and install Zabbix Agent 2
+wget https://repo.zabbix.com/zabbix/6.0/rhel/$version/x86_64/zabbix-release-6.0-1.el$version.noarch.rpm
+rpm -Uvh zabbix-release-6.0-1.el$version.noarch.rpm
+yum install -y zabbix-agent2
+
+# Configure Zabbix Agent 2
+sed -i "s/Server=127.0.0.1/Server=$server/g" /etc/zabbix/zabbix_agentd.conf
+sed -i "s/ServerActive=127.0.0.1/ServerActive=$server_active/g" /etc/zabbix/zabbix_agentd.conf
+sed -i "s/Hostname=Zabbix server/Hostname=$hostname/g" /etc/zabbix/zabbix_agentd.conf
+
+# Start Zabbix Agent 2
+systemctl start zabbix-agent2
+systemctl enable zabbix-agent2
+
+# Clean up
+rm -f zabbix-release-6.0-1.el$version.noarch.rpm
+
+echo "Zabbix Agent 2 has been successfully installed and configured"
