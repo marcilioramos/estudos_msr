3d34c58fc21bb7a49a85dc24779874e232cab4b4
marcilioramos <48597831+marcilioramos@users.noreply.github.com>
2023-06-14 12:07:35 -0300

    Update mudar_nome_host_proxmox.sh

--- virtualização/proxmoxmox/mudar_nome_host_proxmox.sh
+++ virtualização/proxmoxmox/mudar_nome_host_proxmox.sh
@@ -6,14 +6,17 @@ systemctl stop pvedaemon.service
 systemctl stop pveproxy.service
 systemctl stop pvestatd.service
 
+# Receber o novo nome do host
+read -p "Digite o novo nome do host: " new_hostname
+
 # Atualizar o arquivo /etc/hostname
-echo "msr02.home.msr01" > /etc/hostname
+echo "$new_hostname" > /etc/hostname
 
 # Atualizar o arquivo /etc/hosts
-sed -i 's/^\(127\.0\.1\.1\s\).*$/\1msr02.home.msr01/' /etc/hosts
+sed -i "s/^\(127\.0\.1\.1\s\).*$/\1$new_hostname/" /etc/hosts
 
 # Atualizar o nome do nó no cluster
-pvecm setname msr02.home.msr01
+pvecm setname $new_hostname
 
 # Reiniciar o nó
 reboot
